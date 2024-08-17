cb3879ef60f7fdc45dff597ccc7f1c14904cd31b
adityatambi <adityatambi10@gmail.com>
2024-08-17 12:13:25 +0530

    route set-up to products page

---
+++ src/pages/ProductsPage.tsx
@@ -0,0 +1,17 @@
+import { Products } from '../data/products';
+
+export function ProductsPage() {
+  return (
+    <div className="text-center p-5">
+      <h2 className="text-xl font-bold text-slate-600">Here are some great tools for React</h2>
+
+      <ul className="list-none m-0 p-0">
+        {Products.map((product) => (
+          <li key={product.id} className="p-1 text-base text-slate-800">
+            {product.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
